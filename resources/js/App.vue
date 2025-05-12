e7afabfa2ca1216ca6464c94e41601c3b4724ffe
joma0 <joanah.mayor@heig-vd.ch>
2025-05-12 10:17:07 +0200

    First commit

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+import TheHeader from "@/components/TheHeader.vue";
+import PageMain from "@/pages/PageMain.vue";
+const n = ref(1);
+</script>
+
+<template>
+    <div>
+        <TheHeader />
+        <main>
+            <PageMain />
+        </main>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.vue')
+
+@section('title', 'Histoire interactive')
+
+@section('app')
+@vite(['resources/js/app.js'])
+@endsection
